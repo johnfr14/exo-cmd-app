b1c4293f5f7a71fd892e248e042181187c1ac320
johnfr14 <jonathan.alyra@gmail.com>
2021-03-24 12:04:01 +0100

    yass

--- interMajorityTest.js
+++ interMajorityTest.js
@@ -2,11 +2,22 @@ const readline = require('readline-sync')
 
 const information = (majority) => {
   majority = Number(majority)
-  let name = readline.question('Quelle est votre prénom ? : ')
-  let surname = readline.question('Quelle est votre nom ? : ')
-  let age = Number(readline.question('Quelle est votre age ? : '))
 
+  /* STEP 1 */
+  let name = readline.question('Quelle est votre prénom ? : ')
+  while (name.length <= 2 || !isNaN(name) ) {
+    name = readline.question('NOPE ! Quelle est votre prénom ? : ')
+  }
+
+  /* STEP 2 */
+  let surname = readline.question('Quelle est votre nom ? : ')
+  while (surname.length <= 2 || !isNaN(surname)) {
+    surname = readline.question('Quelle est votre nom ? : ')
+  }
+
+  /* STEP 3 */
   /* Check si notre age soit bien un nombre */
+  let age = Number(readline.question('Quelle est votre age ? : '))
   while(isNaN(age)) {
     age = Number(readline.question('Quelle est votre age ? : '))
   }
