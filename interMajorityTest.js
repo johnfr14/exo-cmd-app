0dc0a25a33a9f365bc82e9caf957b5d23d087f2d
johnfr14 <jonathan.alyra@gmail.com>
2021-03-24 12:05:38 +0100

    yass2

--- interMajorityTest.js
+++ interMajorityTest.js
@@ -5,19 +5,21 @@ const information = (majority) => {
 
   /* STEP 1 */
   let name = readline.question('Quelle est votre prénom ? : ')
+  /* Check si notre name soit bien un nom */
   while (name.length <= 2 || !isNaN(name) ) {
     name = readline.question('NOPE ! Quelle est votre prénom ? : ')
   }
 
   /* STEP 2 */
   let surname = readline.question('Quelle est votre nom ? : ')
+  /* Check si notre surname soit bien un nom */
   while (surname.length <= 2 || !isNaN(surname)) {
     surname = readline.question('Quelle est votre nom ? : ')
   }
 
   /* STEP 3 */
-  /* Check si notre age soit bien un nombre */
   let age = Number(readline.question('Quelle est votre age ? : '))
+  /* Check si notre age soit bien un nombre */
   while(isNaN(age)) {
     age = Number(readline.question('Quelle est votre age ? : '))
   }
